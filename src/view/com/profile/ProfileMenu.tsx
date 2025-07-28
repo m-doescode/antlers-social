orual/deer-social

custom user controlled verifiers (mvp)
commit cde467e57e5da3862f3394d15c6ff64c248445f5
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 04:34:58 2025 -0500

    correct empty display name handling

commit 002f6bb99cebf31769c5cc628d329b86e9c63675
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 04:30:16 2025 -0500

    fix list padding

commit ae984e2568a9ddf2717db02c784ac71c46312f55
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 04:15:34 2025 -0500

    drop log and custom remove mutation

commit acddc1c704377e85b734bd1876e2a2cc6002a41f
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 04:07:48 2025 -0500

    custom verification add mutation logic for deer

commit a4b3d31ea9ee2e6eab85cc321e654d4c76bea5e7
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 03:26:35 2025 -0500

    lru cache for verification and service url

commit 6249d5bb424b8dac53c13c9e6aeb164e052dfc8d
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 03:12:15 2025 -0500

    make size power of 2

commit 1dc64ebeb1158cf77cdedb32c976e3719aa7973e
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 03:05:10 2025 -0500

    lru cache for did documents

commit 9f75bee6f668bac6f58415ebf3eb5d9b70f11be8
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Fri Apr 25 02:05:35 2025 -0500

    simplify a bit

commit 413141195bd5ccbb39229064c61f2b6c2190c981
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Thu Apr 24 16:59:13 2025 -0500

    lift verification logic to not require refetch on handle or name change

commit 27c0700e3ab8e6343af265eb5baa8b3e9d0185d9
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Thu Apr 24 16:44:05 2025 -0500

    factor trusted verifiers into query key

commit b358f3d5de24412882177b1e8e0e683b61803ef7
Author: Aviva Ruben <aviva@rubenfamily.com>
Date:   Thu Apr 24 16:22:58 2025 -0500

    filter duplicate verifications

commit 9dbffdcf3d5e065398172e0f92ec1bda7e2fc370
Author: Aviva Ruben

src/view/com/profile/ProfileMenu.tsx
@@ -13,6 +13,11 @@ import {toShareUrl} from '#/lib/strings/url-helpers'
 import {logger} from '#/logger'
 import {type Shadow} from '#/state/cache/types'
 import {useModalControls} from '#/state/modals'
+import {
+  useDeerVerificationEnabled,
+  useDeerVerificationTrusted,
+  useSetDeerVerificationTrust,
+} from '#/state/preferences/deer-verification'
 import {useDevModeEnabled} from '#/state/preferences/dev-mode'
 import {
   RQKEY as profileQueryKey,
@@ -67,6 +72,10 @@ let ProfileMenu = ({
   const isLabelerAndNotBlocked = !!profile.associated?.labeler && !isBlocked
   const [devModeEnabled] = useDevModeEnabled()
   const verification = useFullVerificationState({profile})
+
+  const deerVerificationEnabled = useDeerVerificationEnabled()
+  const deerVerificationTrusted = useDeerVerificationTrusted().has(profile.did)
+  const setDeerVerificationTrust = useSetDeerVerificationTrust()
 
   const [queueMute, queueUnmute] = useProfileMuteMutationQueue(profile)
   const [queueBlock, queueUnblock] = useProfileBlockMutationQueue(profile)
@@ -290,6 +299,31 @@ let ProfileMenu = ({
                   </Menu.ItemText>
                   <Menu.ItemIcon icon={List} />
                 </Menu.Item>
+                {!isSelf &&
+                  deerVerificationEnabled &&
+                  (deerVerificationTrusted ? (
+                    <Menu.Item
+                      testID="profileHeaderDropdownVerificationTrustRemoveButton"
+                      label={_(msg`Remove trust`)}
+                      onPress={() =>
+                        setDeerVerificationTrust.remove(profile.did)
+                      }>
+                      <Menu.ItemText>
+                        <Trans>Remove trust</Trans>
+                      </Menu.ItemText>
+                      <Menu.ItemIcon icon={CircleX} />
+                    </Menu.Item>
+                  ) : (
+                    <Menu.Item
+                      testID="profileHeaderDropdownVerificationTrustAddButton"
+                      label={_(msg`Trust verifier`)}
+                      onPress={() => setDeerVerificationTrust.add(profile.did)}>
+                      <Menu.ItemText>
+                        <Trans>Trust verifier</Trans>
+                      </Menu.ItemText>
+                      <Menu.ItemIcon icon={CircleCheck} />
+                    </Menu.Item>
+                  ))}
                 {verification.viewer.role === 'verifier' &&
                   !verification.profile.isViewer &&
                   (verification.viewer.hasIssuedVerification ? (

m-doescode/antlers-social

src/view/com/profile/ProfileMenu.tsx
@@ -16,6 +16,11 @@ import {isWeb} from '#/platform/detection'
 import {type Shadow} from '#/state/cache/types'
 import {useModalControls} from '#/state/modals'
 import {
+  useDeerVerificationEnabled,
+  useDeerVerificationTrusted,
+  useSetDeerVerificationTrust,
+} from '#/state/preferences/deer-verification'
+import {
   RQKEY as profileQueryKey,
   useProfileBlockMutationQueue,
   useProfileFollowMutationQueue,
@@ -77,6 +82,10 @@ let ProfileMenu = ({
   const [devModeEnabled] = useDevMode()
   const verification = useFullVerificationState({profile})
   const canGoLive = useCanGoLive(currentAccount?.did)
+
+  const deerVerificationEnabled = useDeerVerificationEnabled()
+  const deerVerificationTrusted = useDeerVerificationTrusted().has(profile.did)
+  const setDeerVerificationTrust = useSetDeerVerificationTrust()
 
   const [queueMute, queueUnmute] = useProfileMuteMutationQueue(profile)
   const [queueBlock, queueUnblock] = useProfileBlockMutationQueue(profile)
@@ -328,6 +337,31 @@ let ProfileMenu = ({
                     <Menu.ItemIcon icon={LiveIcon} />
                   </Menu.Item>
                 )}
+                {!isSelf &&
+                  deerVerificationEnabled &&
+                  (deerVerificationTrusted ? (
+                    <Menu.Item
+                      testID="profileHeaderDropdownVerificationTrustRemoveButton"
+                      label={_(msg`Remove trust`)}
+                      onPress={() =>
+                        setDeerVerificationTrust.remove(profile.did)
+                      }>
+                      <Menu.ItemText>
+                        <Trans>Remove trust</Trans>
+                      </Menu.ItemText>
+                      <Menu.ItemIcon icon={CircleXIcon} />
+                    </Menu.Item>
+                  ) : (
+                    <Menu.Item
+                      testID="profileHeaderDropdownVerificationTrustAddButton"
+                      label={_(msg`Trust verifier`)}
+                      onPress={() => setDeerVerificationTrust.add(profile.did)}>
+                      <Menu.ItemText>
+                        <Trans>Trust verifier</Trans>
+                      </Menu.ItemText>
+                      <Menu.ItemIcon icon={CircleCheckIcon} />
+                    </Menu.Item>
+                  ))}
                 {verification.viewer.role === 'verifier' &&
                   !verification.profile.isViewer &&
                   (verification.viewer.hasIssuedVerification ? (