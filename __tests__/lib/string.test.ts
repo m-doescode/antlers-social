e6d00ff44ce7e66bc56f71beae2e491ec672492a
maelstrom <maelstrom@maelstrom.dev>
2025-07-28 23:03:40 +0200

    fix: tests to use deer.social hostname

--- __tests__/lib/string.test.ts
+++ __tests__/lib/string.test.ts
@@ -228,11 +228,11 @@ describe('cleanError', () => {
 describe('toNiceDomain', () => {
   const inputs = [
     'https://example.com/index.html',
-    'https://bsky.app',
+    'https://deer.social',
     'https://bsky.social',
     '#123123123',
   ]
-  const outputs = ['example.com', 'bsky.app', 'Bluesky Social', '#123123123']
+  const outputs = ['example.com', 'deer.social', 'Bluesky Social', '#123123123']
 
   it("displays the url's host in a easily readable manner", () => {
     for (let i = 0; i < inputs.length; i++) {
@@ -244,16 +244,16 @@ describe('toNiceDomain', () => {
 
 describe('toShortUrl', () => {
   const inputs = [
-    'https://bsky.app',
-    'https://bsky.app/3jk7x4irgv52r',
-    'https://bsky.app/3jk7x4irgv52r2313y182h9',
+    'https://deer.social',
+    'https://deer.social/3jk7x4irgv52r',
+    'https://deer.social/3jk7x4irgv52r2313y182h9',
     'https://very-long-domain-name.com/foo',
     'https://very-long-domain-name.com/foo?bar=baz#andsomemore',
   ]
   const outputs = [
-    'bsky.app',
-    'bsky.app/3jk7x4irgv52r',
-    'bsky.app/3jk7x4irgv52...',
+    'deer.social',
+    'deer.social/3jk7x4irgv52r',
+    'deer.social/3jk7x4irgv52...',
     'very-long-domain-name.com/foo',
     'very-long-domain-name.com/foo?bar=baz#...',
   ]
@@ -267,11 +267,11 @@ describe('toShortUrl', () => {
 })
 
 describe('toShareUrl', () => {
-  const inputs = ['https://bsky.app', '/3jk7x4irgv52r', 'item/test/123']
+  const inputs = ['https://deer.social', '/3jk7x4irgv52r', 'item/test/123']
   const outputs = [
-    'https://bsky.app',
-    'https://bsky.app/3jk7x4irgv52r',
-    'https://bsky.app/item/test/123',
+    'https://deer.social',
+    'https://deer.social/3jk7x4irgv52r',
+    'https://deer.social/item/test/123',
   ]
 
   it('appends https, when not present', () => {
@@ -442,58 +442,69 @@ describe('parseEmbedPlayerFromUrl', () => {
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=1',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=1',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=1',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=1',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_short',
       source: 'youtubeShorts',
       hideDetails: true,
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
     {
       type: 'youtube_video',
       source: 'youtube',
-      playerUri: 'https://bsky.app/iframe/youtube.html?videoId=videoId&start=0',
+      playerUri:
+        'https://deer.social/iframe/youtube.html?videoId=videoId&start=0',
     },
 
     undefined,
@@ -896,7 +907,7 @@ describe('createStarterPackLinkFromAndroidReferrer', () => {
 })
 
 describe('parseStarterPackHttpUri', () => {
-  const baseUri = 'https://bsky.app/start'
+  const baseUri = 'https://deer.social/start'
 
   it('returns a valid at uri when http uri is valid', () => {
     const validHttpUri = `${baseUri}/haileyok.com/rkey`
@@ -934,19 +945,19 @@ describe('parseStarterPackHttpUri', () => {
   })
 
   it('returns null when the route is not /start or /starter-pack', () => {
-    const validHttpUri = 'https://bsky.app/start/haileyok.com/rkey'
+    const validHttpUri = 'https://deer.social/start/haileyok.com/rkey'
     expect(parseStarterPackUri(validHttpUri)).toEqual({
       name: 'haileyok.com',
       rkey: 'rkey',
     })
 
-    const validHttpUri2 = 'https://bsky.app/starter-pack/haileyok.com/rkey'
+    const validHttpUri2 = 'https://deer.social/starter-pack/haileyok.com/rkey'
     expect(parseStarterPackUri(validHttpUri2)).toEqual({
       name: 'haileyok.com',
       rkey: 'rkey',
     })
 
-    const invalidHttpUri = 'https://bsky.app/profile/haileyok.com/rkey'
+    const invalidHttpUri = 'https://deer.social/profile/haileyok.com/rkey'
     expect(parseStarterPackUri(invalidHttpUri)).toEqual(null)
   })
 
