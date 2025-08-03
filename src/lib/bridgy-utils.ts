489547c6a8584f293060b4b9e1ee63af466926ad
maelstrom <maelstrom@maelstrom.dev>
2025-08-04 01:09:55 +0200

    fix: not all paragraphs loading from fedi post

--- src/lib/bridgy-utils.ts
+++ src/lib/bridgy-utils.ts
@@ -5,16 +5,20 @@ const NODE_TYPE_ELEMENT = 1
 const NODE_TYPE_TEXT = 3
 
 function emitFacet(node: any, startText: string, facets: Facet[]): string {
+  console.log(node)
   if (node.nodeType == NODE_TYPE_ELEMENT) {
     if (node.tagName == 'br') {
       return '\n'
     } else if (node.tagName == 'a') {
       const resultText = emitFacetList(node.childNodes, startText, facets)
+      // Can't just use startText.length because of utf-8 encoding
+      const byteStart = new Blob([startText]).size
+      const byteEnd = byteStart + new Blob([resultText]).size
       facets.push({
         $type: 'app.bsky.richtext.facet',
         index: {
-          byteStart: startText.length,
-          byteEnd: startText.length + resultText.length,
+          byteStart: byteStart,
+          byteEnd: byteEnd,
         },
         features: [
           {
@@ -24,6 +28,12 @@ function emitFacet(node: any, startText: string, facets: Facet[]): string {
         ],
       })
       return resultText
+    } else if (node.tagName == 'p') {
+      // Paragraph tags should be separated from other elements by two new lines
+      let text = ''
+      if (startText != '' && !startText.endsWith('\n')) text = '\n\n'
+      text += emitFacetList(node.childNodes, startText + text, facets)
+      return text + '\n\n'
     } else {
       return emitFacetList(node.childNodes, startText, facets)
     }
@@ -52,7 +62,15 @@ export function parseMastodonRichText(sourceText: string): RichText {
 
   const parser = new DOMParser()
   const result = parser.parseFromString(sourceText)
-  strippedText = emitFacetList(result.documentElement.childNodes, '', facets)
+  strippedText = emitFacetList(result.childNodes, '', facets)
+
+  // Paragraphs will add trailing newlines. When getting rid of them, we also have to account for facets, so we clamp the last facet
+  strippedText = strippedText.trimEnd()
+  if (facets.length > 0) {
+    const facet = facets[facets.length - 1]
+    const byteMax = new Blob([strippedText]).size
+    facet.index.byteEnd = Math.min(facet.index.byteEnd, byteMax)
+  }
 
   return new RichText({
     text: strippedText,
